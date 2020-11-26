41246a2513c9541ff4d6fc16da6ec0c114ae84da
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-26 16:20:55 +0530

    FLUT-3010 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/flutter_swiperestriction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_swiperestriction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(SwipeSelection());
+
+class SwipeSelection extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SwipeSelectionPicker(),
+    );
+  }
+}
+
+class SwipeSelectionPicker extends StatefulWidget {
+  @override
+  SwipeSelectionPickerState createState() => SwipeSelectionPickerState();
+}
+
+class SwipeSelectionPickerState extends State<SwipeSelectionPicker> {
+  bool _swipeSelection;
+  String _buttonText;
+
+  @override
+  @override
+  void initState() {
+    _swipeSelection = true;
+    _buttonText='Disable swipe selection';
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 70),
+            child: RaisedButton(
+              child: Text(_buttonText),
+              onPressed: () {
+                setState(() {
+                  if(_buttonText=='Disable swipe selection')
+                    {
+                      _buttonText='Enable swipe selection';
+                      _swipeSelection = false;
+                    }
+
+                  else
+                    {
+                      _buttonText='Disable swipe selection';
+                      _swipeSelection = true;
+                    }
+                });
+              },
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.fromLTRB(40, 80, 40,100),
+            child: SfDateRangePicker(
+              view: DateRangePickerView.month,
+              selectionMode: DateRangePickerSelectionMode.range,
+              monthViewSettings: DateRangePickerMonthViewSettings(
+                  enableSwipeSelection: _swipeSelection),
+            ),
+          ),
+        ],
+      ),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
